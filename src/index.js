957f6b052116512aad1d6c46830765c58b177eb3
Farnalsp <farhanalam1221@gmail.com>
2024-10-31 14:40:21 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store-={TodoStore}>
+    <Provider store={TodoStore}>
     <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
